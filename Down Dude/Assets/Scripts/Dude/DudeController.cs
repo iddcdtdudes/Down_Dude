aa7da448f7f5d7906d6e7dade982706896bf76bd
Guy Sirithong <guy_s@sci.tu.ac.th>
2019-02-06 20:16:18 +0700

    Added Main Scene
    + added a simple character for mobile remote testing
    + expanded .gitignore

---
+++ Down Dude/Assets/Scripts/Dude/DudeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeController : MonoBehaviour {
+
+    private void Update()
+    {
+        if(Input.touchCount > 0) {
+
+            Touch touch = Input.GetTouch(0);
+
+            Vector2 position = Camera.main.ScreenToWorldPoint(touch.position);
+
+            transform.position = position + new Vector2(0f, 0.5f);
+        }
+
+    }
+}
