b4980b880e505d447aa9abe990baa4d635042d5f
Guy Sirithong <guy_s@sci.tu.ac.th>
2019-02-12 18:15:27 +0700

    Added Test Obstacles
    + added Obstacle and ObstacleTrigger
    + obstacle trigger will activate binded obstacles after seconds specified for each obstacle

---
+++ Down Dude/Assets/Scripts/Level/Obstacle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Obstacle : MonoBehaviour
+//
+// holds obstacle information
+
+public class Obstacle : MonoBehaviour {
+
+    [SerializeField] private float m_spawnDelay;          // spawn delay after trigger
+
+    public float GetSpawnDelay()
+    {
+        return m_spawnDelay;
+    }
+}
---
+++ Down Dude/Assets/Scripts/Level/ObstacleTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ObstacleTrigger : MonoBehaviour
+//
+// activates listed obstacles when on triggered by Dude
+
+[RequireComponent(typeof(Collider2D))]
+public class ObstacleTrigger : MonoBehaviour {
+
+    [SerializeField] private Obstacle[] m_obstacles;
+
+    private bool isTriggered = false;
+
+    private void Start()
+    {
+        // inactivate all obstacles
+        foreach(Obstacle obstacle in m_obstacles) {
+            obstacle.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        // trigger by Dude
+        if(!isTriggered && collider.CompareTag("Dude")) {
+            isTriggered = true;
+
+            // activate obstacles
+            foreach(Obstacle obstacle in m_obstacles) {
+                StartCoroutine(DelayedActivateObstable(obstacle));
+            }
+        }
+    }
+
+    // activate obstacle after delay seconds specified in the Obstacle component
+    private IEnumerator DelayedActivateObstable(Obstacle obstacle)
+    {
+        yield return new WaitForSeconds(obstacle.GetSpawnDelay());
+        obstacle.gameObject.SetActive(true);
+    }
+}
