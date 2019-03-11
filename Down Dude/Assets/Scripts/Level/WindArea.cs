98ab27b1da751bbcf4b57a11e0086123184cbbee
Guy Sirithong <guy_s@sci.tu.ac.th>
2019-03-11 21:01:26 +0700

    Added Wind Area
    + added wind area that accelerates dude more when in parachute mode

---
+++ Down Dude/Assets/Scripts/Level/WindArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// WindArea : StaticObstacle
+//
+// accelerates dude toward a certain direction when in the area
+// if dude is in parachute mode, dude accelerates more
+
+public class WindArea : StaticObstacle
+{
+    [SerializeField] private Vector2 wind;
+    [SerializeField] private float jetpackForce;
+    [SerializeField] private float parachuteForce;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Dude")) {
+            if(DudeController.instance.GetDudeMode() == DudeMode.PARACHUTE) {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(parachuteForce * wind, ForceMode2D.Force);
+            } else if(DudeController.instance.GetDudeMode() == DudeMode.JETPACK) {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(jetpackForce * wind, ForceMode2D.Force);
+            }
+        }
+    }
+}
