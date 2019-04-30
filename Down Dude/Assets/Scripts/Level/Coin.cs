513d4c8178e7c41a231a6f7eebe1cbe75f1e5ad3
Guy Sirithong <guy_s@sci.tu.ac.th>
2019-04-30 20:02:55 +0700

    Added and Implemented Coins
    + added collectable coin prefab
    + added coin change display manager

---
+++ Down Dude/Assets/Scripts/Level/Coin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Dude")) {
+            OnCollectCoin();
+        }
+    }
+
+    private void OnCollectCoin()
+    {
+        // add coin
+        PlayerDataManager.instance.AddCoins(1);
+
+        // display coin change
+        CoinChangeDisplayManager.instance.displayCoinChange(Camera.main.WorldToScreenPoint(transform.position), 1);
+
+        // remove coin
+        Destroy(gameObject);
+    }
+}
